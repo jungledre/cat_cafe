685039ba21dc693946802fdda527b0fc5a91a781
Adrienne <adrie.k@gmail.com>
2015-01-12 00:18:51 -0800

    You can now destroy cats.

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -4,16 +4,6 @@ class CatsController < ApplicationController
     @cats = Cat.all
   end
 
-  def create
-    @cat = Cat.new(cat_params)
-
-    if @cat.save
-      redirect_to @cat
-    else
-      render 'new'
-    end
-  end
-
   def show
     @cat = Cat.find(params[:id])
   end
@@ -35,6 +25,16 @@ class CatsController < ApplicationController
     @cat = Cat.find(params[:id])
   end
 
+  def create
+    @cat = Cat.new(cat_params)
+
+    if @cat.save
+      redirect_to @cat
+    else
+      render 'new'
+    end
+  end
+
   def update
     @cat = Cat.find(params[:id])
     if @cat.update(cat_params)
@@ -44,10 +44,15 @@ class CatsController < ApplicationController
     end
   end
 
+  def destroy
+    @cat = Cat.find(params[:id])
+    @cat.destroy
+
+    redirect_to cats_path
+  end
+
   private
   def cat_params
     params.require(:cat).permit(:name, :desc, :img)
   end
-
-
 end
